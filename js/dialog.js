ab3f44dbad0f6a16a9407d616f77445bdcbe1c80
李士强 <lishiqiang@didichuxing.com>
2016-11-30 23:06:06 +0800

    add dialog

---
+++ js/dialog.js
@@ -0,0 +1,32 @@
+/*<div class="dialog_confirm">
+		<div class="mask"></div>
+		<div class="dialogCon dialog_duihuanCon text-cen">
+			<img class="dialog_waring" src="/images/dialog_waring.png" alt="">
+			<input class="duihuanInp" placeholder="输入充值手机号" type="text" />
+			<p class="fz12 waring mt10 mb20 col-e09e00">
+				您确定用15000体力值兑换50元话费？
+			</p>
+			<div class="dialog_button inline-block">
+				<button class="determine">确定</button>
+				<button class="cancel">取消</button>
+			</div>
+		</div>
+	</div>*/
+/*
+function dialogConfirm(o){
+	var mask = $('<div class="dialog_confirm">\
+		<div class="mask"></div>\
+		<div class="dialogCon dialog_duihuanCon text-cen">\
+			<img class="dialog_waring" src="/images/dialog_waring.png" alt="">\
+			<input class="duihuanInp" placeholder="输入充值手机号" type="text" />\
+			<p class="fz12 waring mt10 mb20 col-e09e00">\
+				您确定用15000体力值兑换50元话费？\
+			</p>\
+			<div class="dialog_button inline-block">\
+				<button class="determine">确定</button>\
+				<button class="cancel">取消</button>\
+			</div>\
+		</div>\
+	</div>')
+	$(".content").append
+}*/
