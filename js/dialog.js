c7559aa4c04530aa32d101ef365d98ca4dc4ceb8
李士强 <lishiqiang@didichuxing.com>
2016-12-01 08:35:42 +0800

    新增弹窗js

--- js/dialog.js
+++ js/dialog.js
@@ -13,20 +13,31 @@
 		</div>
 	</div>*/
 /*
+
+			
+			
+ */
 function dialogConfirm(o){
-	var mask = $('<div class="dialog_confirm">\
+	var $mask = $('<div class="dialog_confirm">\
 		<div class="mask"></div>\
-		<div class="dialogCon dialog_duihuanCon text-cen">\
+		<div class="_dialogCon text-cen">\
 			<img class="dialog_waring" src="/images/dialog_waring.png" alt="">\
-			<input class="duihuanInp" placeholder="输入充值手机号" type="text" />\
-			<p class="fz12 waring mt10 mb20 col-e09e00">\
-				您确定用15000体力值兑换50元话费？\
-			</p>\
+			<div class="dialogCon">\
+			</div>\
 			<div class="dialog_button inline-block">\
 				<button class="determine">确定</button>\
 				<button class="cancel">取消</button>\
 			</div>\
 		</div>\
 	</div>')
-	$(".content").append
-}*/
+	$mask.find(".dialogCon").addClass(o.className).append(o.content)
+	$(".content").append($mask)
+
+	$mask.find(".dialog_button .determine").on("click",function(){
+		o.autohiden!=false&&$(".dialog_confirm").remove();
+		o.callback&&o.callback();
+	})
+	$mask.find(".dialog_button .cancel").on("click",function(){
+		$(".dialog_confirm").remove();
+	})
+}
